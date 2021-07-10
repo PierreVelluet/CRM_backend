7275645edd1f230138a1d3de19a2aa500ae83b09
PierreVelluet <velluetp@gmail.com>
2021-07-10 23:58:42 +0400

    auth done

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const usersRoute = require("./user");
+const authRoute = require("./auth")
 
 router.use('/user', usersRoute)
+router.use('/auth', authRoute)
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+
+const passport = require("./passport/setup");
+
 const app = express();
-const bodyParser = require("body-parser")
 
 const apiRoutes = require("./api");
 
-app.use(bodyParser.json());
+mongoose
+    .connect(process.env.DB_CONFIG, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    session({
+        secret: "very secret this is",
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: process.env.DB_CONFIG })
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/api", apiRoutes);
-
 
 app.listen(3000);
